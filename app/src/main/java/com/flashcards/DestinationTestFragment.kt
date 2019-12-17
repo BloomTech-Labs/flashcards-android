751132564251b7b6d2799f2b6bdc9e491e07b9e1
patrick <patrickchow0803@gmail.com>
2019-12-17 17:38:59 -0500

    Added proper pop back to main functionality.

--- app/src/main/java/com/flashcards/DestinationTestFragment.kt
+++ app/src/main/java/com/flashcards/DestinationTestFragment.kt
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_destination_test.*
 
 
 class DestinationTestFragment : Fragment() {
@@ -17,6 +19,13 @@ class DestinationTestFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_destination_test, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_return.setOnClickListener {
+            findNavController().navigate(R.id.action_destinationTestFragment_to_homeTestFragment)
+        }
     }
 }
 
