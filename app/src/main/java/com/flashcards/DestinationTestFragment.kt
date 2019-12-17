03083888c0b3258f16a1d0a0953dac6c949a7495
patrick <patrickchow0803@gmail.com>
2019-12-17 13:59:08 -0500

    Added 2 fragments for testing navigation components.

---
+++ app/src/main/java/com/flashcards/DestinationTestFragment.kt
@@ -0,0 +1,22 @@
+package com.flashcards
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class DestinationTestFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_destination_test, container, false)
+    }
+}
+
