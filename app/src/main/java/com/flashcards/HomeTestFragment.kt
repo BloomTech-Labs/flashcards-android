e5e862a86cb425e3c08b7f8992e1e80b999ed2d4
patrick <patrickchow0803@gmail.com>
2019-12-17 14:18:23 -0500

    Got navigation working from home fragment to destination fragment

--- app/src/main/java/com/flashcards/HomeTestFragment.kt
+++ app/src/main/java/com/flashcards/HomeTestFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_home_test.*
 
 class HomeTestFragment : Fragment() {
 
@@ -13,9 +15,16 @@ class HomeTestFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home_test, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_destination.setOnClickListener{
+            findNavController().navigate(R.id.action_homeTestFragment_to_destinationTestFragment)
+        }
+
+    }
 
 }
