b0c12a6c9ade78ea21b806957f12bb8e5a3c12c3
patrick <patrickchow0803@gmail.com>
2019-12-18 16:22:31 -0500

    Added Google Login Functionality.

--- app/src/main/java/com/lambda/mnemecards/HomeTestFragment.kt
+++ app/src/main/java/com/lambda/mnemecards/HomeTestFragment.kt
@@ -1,25 +1,58 @@
 package com.lambda.mnemecards
 
 
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.auth.GoogleAuthException
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.fragment_home_test.*
 
 class HomeTestFragment : Fragment() {
+
+    lateinit var fragmentContext: Context
+    var googleSignInClient: GoogleSignInClient ?= null
+    val RC_SIGN_IN = 1000
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        fragmentContext = container!!.context
         return inflater.inflate(R.layout.fragment_home_test, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(fragmentContext, gso)
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
+        btn_google_login.setOnClickListener {
+
+            signIn()
+        }
 
         btn_destination.setOnClickListener{
             if((!et_first.text.toString().isNullOrEmpty()) && (!et_second.text.toString().isNullOrEmpty())){
@@ -29,4 +62,40 @@ class HomeTestFragment : Fragment() {
         }
     }
 
+    private fun signIn() {
+        val signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == RC_SIGN_IN){
+
+            var task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+            }
+        }
+    }
+
+    fun firebaseAuthWithGoogle(acct: GoogleSignInAccount){
+        var credential = GoogleAuthProvider.getCredential(acct?.idToken, null)
+        FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener{task ->
+            if(task.isSuccessful){
+                println("Google Login Success")
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+    }
 }
--- app/src/main/java/com/lambda/mnemecards/MainActivity.kt
+++ app/src/main/java/com/lambda/mnemecards/MainActivity.kt
@@ -2,14 +2,23 @@ package com.lambda.mnemecards
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        Toast.makeText(this, "Firebase Connection Test", Toast.LENGTH_SHORT).show()
+
+
+
 
         //This makes it so that the when moving between fragments, there's a back button now.
         //Nav Host Fragment is the ID I gave to the fragment in the activity_main.xml
