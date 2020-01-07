6bb3ca89b82be5cf247af439afac0a60081b0aec
patrick <patrickchow0803@gmail.com>
2020-01-07 11:22:19 -0500

    Added a Deck Class

---
+++ app/src/main/java/com/lambda/mnemecards/network/Deck.kt
@@ -0,0 +1,10 @@
+package com.lambda.mnemecards.network
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Deck(
+    val name: String,
+    val cards: List<Card>
+): Parcelable
