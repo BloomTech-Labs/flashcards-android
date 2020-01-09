2fbc16f0e6d49a1f96b8991a483831994794fbda
patrick <patrickchow0803@gmail.com>
2020-01-08 21:36:22 -0500

    Settings Fragment now has option bar

--- app/src/main/java/com/lambda/mnemecards/settings/SettingsFragment.kt
+++ app/src/main/java/com/lambda/mnemecards/settings/SettingsFragment.kt
@@ -2,10 +2,9 @@ package com.lambda.mnemecards.settings
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 import com.lambda.mnemecards.R
 
@@ -18,8 +17,28 @@ class SettingsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        setHasOptionsMenu(true)
+        
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    /**
+     * Inflates the overflow menu that contains filtering options.
+     */
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.overflow_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.settings -> findNavController().navigate(R.id.action_homeFragment_to_settingsFragment)
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
 
