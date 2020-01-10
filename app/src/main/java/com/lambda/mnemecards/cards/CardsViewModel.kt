e9ccff293ff39eb4fc343d062c08e8b4391a48cd
patrick <patrickchow0803@gmail.com>
2020-01-10 16:27:08 -0500

    Added a basic view model

---
+++ app/src/main/java/com/lambda/mnemecards/cards/CardsViewModel.kt
@@ -0,0 +1,26 @@
+package com.lambda.mnemecards.cards
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.lambda.mnemecards.network.Deck
+
+/**
+ *  The [ViewModel] associated with the [HomeFragment], containing information about the selected
+ *  [Deck].
+ */
+
+class CardsViewModel(deck: Deck, app: Application):AndroidViewModel(app){
+
+    private val _selectedDeck = MutableLiveData<Deck>()
+
+    val selectedDeck:LiveData<Deck>
+        get() = _selectedDeck
+
+    init {
+        _selectedDeck.value = deck
+    }
+
+}
