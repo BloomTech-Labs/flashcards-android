1ffbaaa196516a08810545172950d4b57b33bd8f
patrick <patrickchow0803@gmail.com>
2020-01-13 20:26:10 -0500

    Added a User class to hold their preferences.

---
+++ app/src/main/java/com/lambda/mnemecards/network/User.kt
@@ -0,0 +1,18 @@
+package com.lambda.mnemecards.network
+
+import com.google.firebase.firestore.PropertyName
+
+data class User (
+    val id: String,
+    var favSubjects: String?,
+    var MobileOrDesktop: String?,
+    var customOrPremade: String?,
+
+    @PropertyName("notification-frequency")
+    var notificationFrequency: String?,
+
+    @PropertyName("study-frequency")
+    var studyFrequency: String?,
+
+    var techniques: String?
+)
