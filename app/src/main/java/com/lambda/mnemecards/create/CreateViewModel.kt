14921766b8424fd1abec3ba689a505d37e79b22d
patrick <patrickchow0803@gmail.com>
2020-01-18 17:40:13 -0500

    Added the top of the views for the create xml

--- app/src/main/java/com/lambda/mnemecards/create/CreateViewModel.kt
+++ app/src/main/java/com/lambda/mnemecards/create/CreateViewModel.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.lambda.mnemecards.network.Card
+import com.lambda.mnemecards.network.DataX
 import com.lambda.mnemecards.network.Deck
 
 class CreateViewModel(decks: MutableList<Deck>, app: Application) : AndroidViewModel(app){
@@ -11,6 +13,8 @@ class CreateViewModel(decks: MutableList<Deck>, app: Application) : AndroidViewM
     private val _decks = MutableLiveData<MutableList<Deck>>()
     val decks : LiveData<MutableList<Deck>>
         get() = _decks
+
+    var cards = mutableListOf<DataX>()
 
     init {
         this._decks.value = decks
