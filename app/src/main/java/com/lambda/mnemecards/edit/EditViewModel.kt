21813ec6f64f1eeb3f94c200c3f6fc8ea7428056
patrick <patrickchow0803@gmail.com>
2020-01-22 21:00:30 -0500

    Added a basic EditViewModel and a EditViewModelFactory.

---
+++ app/src/main/java/com/lambda/mnemecards/edit/EditViewModel.kt
@@ -0,0 +1,10 @@
+package com.lambda.mnemecards.edit
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import com.lambda.mnemecards.network.Deck
+
+class EditViewModel(deck: Deck, app: Application) : AndroidViewModel(app)  {
+
+}
